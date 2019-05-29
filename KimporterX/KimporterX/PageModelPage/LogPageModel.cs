d44fe0cf8f021ec48259ce3837647683f0f205b8
Yishi_Liu <yishi_liu@buildingpoint.com.au>
2019-05-29 10:39:08 +1000

    log page clear

--- KimporterX/KimporterX/PageModelPage/LogPageModel.cs
+++ KimporterX/KimporterX/PageModelPage/LogPageModel.cs
@@ -6,6 +6,8 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
 
 namespace KimporterX
 {
@@ -13,12 +15,15 @@ namespace KimporterX
     {
         public LogPageModel()
         {
-            BlobCache.UserAccount.GetAllObjects<OperationHistory>().Subscribe(
-                x => LogList = new ObservableCollection<OperationHistory>(x.OrderByDescending(i => i.Time)),
-                ex => LogList = new ObservableCollection<OperationHistory>()
-                );
+            ClearLogCommand = new Command(() =>
+                BlobCache.UserAccount.InvalidateAllObjects<OperationHistory>()
+                .Subscribe( i => LogList = new ObservableCollection<OperationHistory>()));
+            BlobCache.UserAccount.GetAllObjects<OperationHistory>()
+                .Subscribe( x => LogList = new ObservableCollection<OperationHistory>(x.OrderByDescending(i => i.Time)),
+                    ex => LogList = new ObservableCollection<OperationHistory>());
         }
 
         public ObservableCollection<OperationHistory> LogList { get; set; } = new ObservableCollection<OperationHistory>();
+        public ICommand ClearLogCommand { get; set; }
     }
 }
